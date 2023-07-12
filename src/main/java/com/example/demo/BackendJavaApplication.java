ecdf5c99b7f3519ea7f1ca6587b780dad6de012d
Panupong <6310210308@psu.ac.th>
2023-07-12 12:04:05 +0700

    lab2 getAll,create Employee

--- src/main/java/com/example/demo/BackendJavaApplication.java
+++ src/main/java/com/example/demo/BackendJavaApplication.java
@@ -10,6 +10,7 @@ public class BackendJavaApplication {
 		
 		SpringApplication.run(BackendJavaApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Spring boot");
 	}
 
 }
