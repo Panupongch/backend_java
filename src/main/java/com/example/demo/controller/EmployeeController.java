f4d7148ff8e43eb27ed2ba4e4352fc03510627e5
Panupong <6310210308@psu.ac.th>
2023-09-20 13:05:44 +0700

    lab10 cors

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.parser.Part.IgnoreCaseType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.example.demo.repository.SkillRepository;
 import jakarta.servlet.ServletOutputStream;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 	
 	@Autowired
